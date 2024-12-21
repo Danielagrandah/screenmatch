5fe4d980c11bed1a481e3acf4ee0de85b68658f8
danielagrandah <karen.grandah@gmail.com>
2024-12-21 17:40:17 +0100

    Se crea la clase Principal,  se utiliza la funcion lambda para resumir el condigo for i.

--- src/main/java/com/aluracursos/screenmatch/model/DatosSerie.java
+++ src/main/java/com/aluracursos/screenmatch/model/DatosSerie.java
@@ -1,9 +1,12 @@
 package com.aluracursos.screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 //JsonAlias sirve para poder leer el atributo
-public record DatosSerie(@JsonAlias("title") String titulo,
+public record DatosSerie(@JsonAlias("Title") String titulo,
                          @JsonAlias("totalSeasons") int totalDeTemporadas,
                          @JsonAlias("imdbRating") Double evaluacion) {
 }
---
+++ src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -0,0 +1,52 @@
+package com.aluracursos.screenmatch.principal;
+
+import com.aluracursos.screenmatch.model.DatosEpisodio;
+import com.aluracursos.screenmatch.model.DatosSerie;
+import com.aluracursos.screenmatch.model.DatosTemporadas;
+import com.aluracursos.screenmatch.service.ConsumoAPI;
+import com.aluracursos.screenmatch.service.ConvierteDatos;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner teclado = new Scanner(System.in);
+    private ConsumoAPI consumoApi = new ConsumoAPI();
+    private final String URL_BASE = "http://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=f1e64aca";
+    private ConvierteDatos conversor = new ConvierteDatos();
+
+    public void muestraMenu(){
+        System.out.println("*******************\n"+ "Por favor escribe el nombre de la serie que deaseas buscar: ");
+        // Busca los datos generales de la Serie.
+        var nombreSerie = teclado.nextLine();
+        var json = consumoApi.obtenerDatos(URL_BASE + nombreSerie.replace(" ", "+") + API_KEY);
+        System.out.println(json);
+        var datos = conversor.obtenerDatos(json, DatosSerie.class);
+
+        // Busca los datos de todas las temporadas
+        List<DatosTemporadas> temporadas = new ArrayList<>();
+        for (int i = 1; i <= datos.totalDeTemporadas() ; i++) {
+            json = consumoApi.obtenerDatos(URL_BASE + nombreSerie.replace(" ", "+") + "&Season=" + i + API_KEY);
+            var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
+            temporadas.add(datosTemporadas);
+        }
+        //temporadas.forEach(System.out::println);
+
+        //Mostrar solo el titulo de los episodios para las temporadas
+
+        /*for (int i = 0; i < datos.totalDeTemporadas(); i++) {
+            List<DatosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+            for (int j = 0; j < episodiosTemporada.size(); j++) {
+                System.out.println(episodiosTemporada.get(j).titulo());
+
+            }
+
+        }*/
+        // Simplica el fori e imprime el titulo de los episodios para las temporadas
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+
+    }
+}
