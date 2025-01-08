a215b51e812d7bd9479c43bcbc8c63f9e7a62b00
danielagrandah <karen.grandah@gmail.com>
2025-01-08 21:04:42 +0100

    Conectar al front-End, uso de RestCroller, Mapping, Autowired

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -13,42 +13,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.List;
 
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
+public class ScreenmatchApplication {
 	@Autowired
-	private SerieRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
-//		var consumoApi = new ConsumoAPI();
-//		var json = consumoApi.obtenerDatos("http://www.omdbapi.com/?t=game+of+thrones&apikey=f1e64aca");
-//		// Muestra una imagen aleqtoriq
-//		var json = consumoApi.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
-//		System.out.println(json);
-//
-//		ConvierteDatos conversor = new ConvierteDatos();
-//		var datos = conversor.obtenerDatos(json, DatosSerie.class);
-//		System.out.println(datos);
-//
-//		json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=f1e64aca");
-//		DatosEpisodio episodio = conversor.obtenerDatos(json, DatosEpisodio.class);
-//		System.out.println(episodio);
-
-/*		List<DatosTemporadas> temporadas = new ArrayList<>();
-		for (int i = 1; i <= datos.totalDeTemporadas() ; i++) {
-			json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season="+ i +"&apikey=f1e64aca");
-			var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
-			temporadas.add(datosTemporadas);
-		}
-		temporadas.forEach(System.out::println);*/
-	@Override
-	public void run(String... args) throws Exception {
-		Principal principal = new Principal(repository);
-		principal.muestraMenu();
-	/*	EjemploStreams ejemploStreams = new EjemploStreams();
-		ejemploStreams.muestraEjemplo();*/
 
 
-
-	}
 }
---
+++ src/main/java/com/aluracursos/screenmatch/dto/SerieDTO.java
@@ -0,0 +1,19 @@
+package com.aluracursos.screenmatch.dto;
+
+import com.aluracursos.screenmatch.model.Categoria;
+import com.aluracursos.screenmatch.model.Episodio;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+public record SerieDTO (String titulo,
+                        Integer totalDeTemporadas,
+                        Double evaluacion,
+                        String poster,
+                        Categoria genero,
+                        String actores,
+                        String idiomas,
+                        String premios,
+                        String sinopsis){
+
+}
